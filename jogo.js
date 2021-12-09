d9f314004acaeb2f59cd2145f9870d88c031cb67
Caio Ramos <caio.campsv@gmail.com>
2021-12-09 20:00:47 -0300

    Criação dos arquivos iniciais do jogo

---
+++ jogo.js
@@ -0,0 +1,12 @@
+var palavraSecreta = "ornitorrinco";
+
+function montarResultado() {
+
+  palavraSecreta.length
+  for ()var pos = 0; pos < 
+
+}
+
+function clickarLetra(letra) {
+  alert(letra);
+}
