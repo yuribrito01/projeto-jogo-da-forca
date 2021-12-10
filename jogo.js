a703d2a20e312e15c4a38d33ebaf9bca4a4df9d9
Caio Ramos <caio.campsv@gmail.com>
2021-12-09 21:23:25 -0300

    Adiciona comportamento do jogo

--- jogo.js
+++ jogo.js
@@ -1,12 +1,83 @@
-var palavraSecreta = "ornitorrinco";
+var palavraSecreta = "c";
+
+var letrasCertas = [];
+var letrasErradas = [];
 
 function montarResultado() {
+  var resultado = "";
 
-  palavraSecreta.length
-  for ()var pos = 0; pos < 
+  for (var pos = 0; pos < palavraSecreta.length; pos++) {
+    var letraDaVez = palavraSecreta[pos];
 
+    if (letrasCertas.includes(letraDaVez.toUpperCase())) {
+      resultado += " " + letraDaVez;
+    } else {
+      resultado += " _";
+    }
+  }
+
+  var h1 = document.getElementById("palavra_secreta");
+  h1.innerText = resultado;
 }
 
-function clickarLetra(letra) {
-  alert(letra);
+function clickarLetra(botao, letra) {
+
+  if (palavraSecreta.toLowerCase().includes(letra.toLowerCase())) {
+    letrasCertas.push(letra);
+    montarResultado();
+
+    botao.classList.add("letraCerta");
+
+  } else {
+    letrasErradas.push(letra);
+    atualizarImagem();
+
+    botao.classList.add("letraErrada");
+  }
+
+  botao.disabled = true;
+  verificaFimDeJogo();
 }
+
+function verificaFimDeJogo() {
+  if (letrasErradas.length == 6) {
+    window.location.href = "teste_resultado.html";
+  }
+
+  var h1 = document.getElementById("palavra_secreta");
+  if (!h1.innerText.includes("_")) {
+    window.location.href = "teste_resultado.html";
+  }
+}
+
+function atualizarImagem() {
+
+  switch (letrasErradas.length) {
+    case 1:
+      var nomeImagem = "imagens/erro1.png";
+
+      var img = document.getElementById("img");
+      img.src = nomeImagem;
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+    case 5:
+
+      break;
+    case 6:
+
+      break;
+    default:
+
+  }
+}
+
+montarResultado();
